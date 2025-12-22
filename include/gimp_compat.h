c834072c952b491d01c1512c9982be83eecc88b9
dfreitas-git <dfreitas.hh@gmail.com>
2025-12-21 17:00:30 -0800

    Added gimp_compat.h to map guint/guint8/guint16 to uint32_t/uint8_t/uint16_t types so I don't have to edit the Gimp output

---
+++ include/gimp_compat.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include <stdint.h>
+
+// Minimal GLib type compatibility for GIMP C-source exports
+typedef uint8_t  guint8;
+typedef uint16_t guint16;
+typedef uint32_t guint;
+
