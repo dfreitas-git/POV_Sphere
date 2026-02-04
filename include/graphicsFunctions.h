988534620a1ddf0b1f4f518bdd91417c25ec5b4c
dfreitas-git <dfreitas.hh@gmail.com>
2026-02-04 09:07:08 -0800

    Organized graphicsFunctions.h

--- include/graphicsFunctions.h
+++ include/graphicsFunctions.h
@@ -7,13 +7,17 @@
 #include <globals.h>
 #include <math.h>
 
-// Struct to hold the rgb color fields
-struct RGB { uint8_t r, g, b; };
 
+
+//#################################
 // eyeball movement enum
+//#################################
 enum EYEBALL_MOVE { LEFT, RIGHT, UP, DOWN, CENTER };
 
+
+//#################################
 // Pinecrest owl structs
+//#################################
 const uint32_t BLINK_DURATION_MS = 200;   // visible owl blink
 
 enum OwlEventType {
@@ -48,6 +52,13 @@ struct OwlTimeline {
     .squawkStartTime = 0
   };
 
+
+//#################################
+// Color definitions
+//#################################
+// Struct to hold the rgb color fields
+struct RGB { uint8_t r, g, b; };
+
 // Struct to hold the color definitions
 struct palette { 
     RGB white = {255,255,255};
@@ -79,6 +90,24 @@ constexpr RGB spiralColors[] = {
                 {0,0,255},
               };
 
+
+// Set up some pre-determined colors to cycle between (these give good contrast between each pair)
+constexpr RGB colors[] = {
+                {235,0,0},
+                {0,235,0},
+                {1,65,223},
+                {10,229,0},
+                {218,24,94},
+                {74,160,240},
+                {231,121,67},
+                {72,106,16},
+                {101,16,187},
+                {5,17,37},
+                {196,16,12},
+                {0,235,235},
+              };
+
+
 // Struct to hold vertex point
 struct Vec2 {
     int x, y;
@@ -87,7 +116,9 @@ struct Vec2 {
 // Counter that holds the number of times we ever tried to write a pixel outside the framebuffer bounds
 static uint32_t outOfBoundsPixelCount = 0;
 
+//#################################
 // prototypes
+//#################################
 void writePixel(int col, int row, const struct RGB& color);
 void drawSpiral(uint32_t phase, int K, int numSpirals, int thickness, const struct RGB& color);
 void drawLetter(uint8_t (*letter)[7], int llX ,int llY, const struct RGB& bgColor, const struct RGB& fgColor);
@@ -126,7 +157,11 @@ void fillBB_image();
 void fillBB_eyeball();
 void fillBB_pinecrest();
 
-// Sine for each column via look-up-table for speed
+//#################################
+//  Math functions 
+//#################################
+
+// Sine for each column (120 total) by look-up-table for speed
 constexpr float sineLUT[COLUMNS] = {
      0.0,.052,.105,.156,.208,.259,.309,.358,.407,.454,.500,.545,.588,.629,.669,.707,.749,.777,.809,.839,.866,.891,.914,.934,.951,.966,.978,.988,.995,.999, 
      1.0,.999,.995,.988,.978,.966,.951,.934,.914,.891,.866,.839,.809,.777,.749,.707,.669,.629,.588,.545,.500,.454,.407,.358,.309,.259,.208,.156,.105,.052,
@@ -134,23 +169,9 @@ constexpr float sineLUT[COLUMNS] = {
     -1.0,-.999,-.995,-.988,-.978,-.966,-.951,-.934,-.914,-.891,-.866,-.839,-.809,-.777,-.749,-.707,-.669,-.629,-.588,-.545,-.500,-.454,-.407,-.358,-.309,-.259,-.208,-.156,-.105,-.052
 };
 
-
-// Set up some pre-determined colors to cycle between (these give good contrast between each pair)
-constexpr RGB colors[] = {
-                {235,0,0},
-                {0,235,0},
-                {1,65,223},
-                {10,229,0},
-                {218,24,94},
-                {74,160,240},
-                {231,121,67},
-                {72,106,16},
-                {101,16,187},
-                {5,17,37},
-                {196,16,12},
-                {0,235,235},
-              };
-
+//#################################
+// Fonts used by graphicsFunctions
+//#################################
 struct font_7x5 { 
     uint8_t P[7][5] = {{0,0,0,0,0},
                        {0,1,1,0,0},
